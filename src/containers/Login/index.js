fdc44e44876f2d13cb02ed5c815697bb401c3802
phantware <phantmoney2011@gmail.com>
2022-07-16 14:37:05 +0100

    router paths created

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginContainer = () => {
   return (
     <div>
       <h1>LoginContainer</h1>
+      <Link to='/auth/register'>Register here</Link>
     </div>
   )
 }
--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RegisterContainer = () => {
   return (
     <div>
       <h1>RegisterContainer</h1>
+
+      <Link to='/auth/login'>Login here</Link>
     </div>
   )
 }
