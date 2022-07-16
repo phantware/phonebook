46242a2100888745230445ea1557f589dd8eddd3
phantware <phantmoney2011@gmail.com>
2022-07-16 15:49:54 +0100

    axios defined in helper function

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import register from '../../context/actions/registar'
 
 const RegisterContainer = () => {
+  useEffect(() => {
+    register()
+  }, [])
+
   return (
     <div>
       <h1>RegisterContainer</h1>
---
+++ src/context/actions/registar.js
@@ -0,0 +1,10 @@
+import axiosInstance from '../../helpers/axios'
+
+const register = () => {
+  axiosInstance
+    .post('/auth/post')
+    .then((post) => console.log('post', post))
+    .catch((err) => console.log('err', err))
+}
+
+export default register
