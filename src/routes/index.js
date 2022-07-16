2e5e097568e549f1400ec0bb01408d8e4f4b19cd
phantware <phantmoney2011@gmail.com>
2022-07-16 06:41:48 +0100

    router paths created

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const routes = [
+  {
+    path: '/auth/register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
+  {
+    path: '/auth/login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: '/',
+    component: ContactsComponent,
+    title: 'Contacts',
+  },
+  {
+    path: '/contacts/create',
+    component: CreateComponent,
+    title: 'Create Contact',
+  },
+]
